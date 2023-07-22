b97a90d259dd7cda84b1997e3ae943ee6f72ea53
hfsyntax <52133373+hfsyntax@users.noreply.github.com>
2023-07-22 14:53:27 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const path = require('path');
+const keepAlive = require('./server')
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+     GatewayIntentBits.Guilds,
+     GatewayIntentBits.GuildMessages,
+     GatewayIntentBits.MessageContent,
+     GatewayIntentBits.GuildMembers,
+     GatewayIntentBits.GuildModeration
+    ] });
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+  
+  keepAlive()
+  client.login(process.env.TOKEN)
