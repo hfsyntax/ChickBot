16c6f33e2e5d0fbdd2dab6a4e6aadc8261db109c
hfsyntax <52133373+hfsyntax@users.noreply.github.com>
2023-07-22 15:01:42 -0400

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,15 +1,25 @@
-const { Events, EmbedBuilder } = require('discord.js');
+const {
+    Events,
+    EmbedBuilder
+} = require('discord.js');
 
 module.exports = {
-	name: Events.GuildMemberAdd,
-	once: false,
-	execute(member) {
-		const channel = member.guild.channels.cache.find(c => c.name === "logs")
+    name: Events.GuildMemberAdd,
+    once: false,
+    execute(member) {
+        const channel = member.guild.channels.cache.find(c => c.name === "logs")
         const embed = new EmbedBuilder()
-        .setColor("008000")
-        .setAuthor({ name: `${member.user.username} <${member.id}>`, iconURL: member.user.avatarURL() ? member.user.avatarURL() : member.user.defaultAvatarURL})
-        .setTimestamp()
-        .setFooter({ text: "Joined"});
-        channel.send({ embeds: [embed] });
-	},
+            .setColor("008000")
+            .setAuthor({
+                name: `${member.user.username} <${member.id}>`,
+                iconURL: member.user.avatarURL() ? member.user.avatarURL() : member.user.defaultAvatarURL
+            })
+            .setTimestamp()
+            .setFooter({
+                text: "Joined"
+            });
+        channel.send({
+            embeds: [embed]
+        });
+    },
 };
