9c9e8c4c4b78e91c71c1bcf2368c5473c0312c8d
hfsyntax <52133373+hfsyntax@users.noreply.github.com>
2023-07-22 15:02:57 -0400

    Update messageDelete.js

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,21 +1,34 @@
-const { Events, EmbedBuilder } = require('discord.js');
+const {
+    Events,
+    EmbedBuilder
+} = require('discord.js');
 
 module.exports = {
-	name: Events.MessageDelete,
-	once: false,
-	execute(message) {
-		if (!message.guild || message.embeds[0]) return
+    name: Events.MessageDelete,
+    once: false,
+    execute(message) {
+        if (!message.guild || message.embeds[0]) return
         const channel = message.guild.channels.cache.find(c => c.name === "logs")
         const embed = new EmbedBuilder()
-        .setColor("808080")
-        .setAuthor({ name: `${message.author.username} <${message.author.id}>`, iconURL: message.author.avatarURL() })
-        .addFields(
-            { name: 'Channel', value: message.channel.name },
-            { name: 'Message', value: message.content.replace(/@/g, '@ ') },
-        )
-        .setTimestamp()
-        .setFooter({ text: 'Deleted'});
+            .setColor("808080")
+            .setAuthor({
+                name: `${message.author.username} <${message.author.id}>`,
+                iconURL: message.author.avatarURL()
+            })
+            .addFields({
+                name: 'Channel',
+                value: message.channel.name
+            }, {
+                name: 'Message',
+                value: message.content.replace(/@/g, '@ ')
+            }, )
+            .setTimestamp()
+            .setFooter({
+                text: 'Deleted'
+            });
 
-    channel.send({ embeds: [embed] });
-	},
+        channel.send({
+            embeds: [embed]
+        });
+    },
 };
