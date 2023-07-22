1ecf6b7a71affcb270731249f8a93351b7f73b53
hfsyntax <52133373+hfsyntax@users.noreply.github.com>
2023-07-22 15:03:22 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,11 @@
-const { Events } = require('discord.js');
+const {
+    Events
+} = require('discord.js');
 
 module.exports = {
-	name: Events.ClientReady,
-	once: true,
-	async execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
+    name: Events.ClientReady,
+    once: true,
+    async execute(client) {
+        console.log(`Ready! Logged in as ${client.user.tag}`);
+    },
 };
