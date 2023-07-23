bf32a6eed69df0718fd88a78ecb6a89b2f9049cf
hfsyntax <52133373+hfsyntax@users.noreply.github.com>
2023-07-23 17:29:26 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,22 +1,36 @@
-const {
-    Events
-} = require('discord.js');
-const heartbeatInterval = 5 * 60 * 1000; // 5 minutes
 
-function sendHeartbeat(client) {
-    try {
-        console.log(`Heartbeat sent successfully: ${client.ws.ping}`);
-    } catch (error) {
-        console.error('Error sending heartbeat:', error);
-    }
-}
+const { Events, WebhookClient, EmbedBuilder } = require('discord.js');
+const heartbeatInterval = 5 * 60 * 1000; // 5 minutes
+const webhookClient = new WebhookClient({ url: process.env.WEBHOOK_URL });
+
+async function sendHeartbeat(client) {
+	try {
+	  const ping = client.ws.ping;
+	  console.log(`Heartbeat sent successfully: ${ping}`);
+	} catch (error) {
+	  console.error('Error sending heartbeat:', error);
+	  try {
+			const embed = new EmbedBuilder()
+			.setColor("#808080")
+			.setAuthor({ name: `${client.user.username} <${client.user.id}>`, iconURL: client.user.avatarURL() })
+			.setTimestamp()
+        	.setFooter({ text: "Offline"});
+
+			await webhookClient.send({
+				embeds: [embed],
+			});
+		} catch (err) {
+			console.error('Error sending webhook message for the error:', err);
+		}
+	}
+  }
 
 module.exports = {
-    name: Events.ClientReady,
-    once: false,
-    execute(client) {
-        console.log(`Ready! Logged in as ${client.user.tag}`);
-        console.log(`WebSocket Status: ${client.ws.status}`);
-        setInterval(() => sendHeartbeat(client), heartbeatInterval);
-    },
+	name: Events.ClientReady,
+	once: false,
+	execute(client) {
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+		console.log(`WebSocket Status: ${client.ws.status}`);
+		setInterval(() => sendHeartbeat(client), heartbeatInterval);
+	},
 };
