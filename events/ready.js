e84c8159503e05b8ac49c3f3b6bb83f800701cf3
hfsyntax <52133373+hfsyntax@users.noreply.github.com>
2023-07-23 16:58:30 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,22 @@
 const {
     Events
 } = require('discord.js');
+const heartbeatInterval = 5 * 60 * 1000; // 5 minutes
+
+function sendHeartbeat(client) {
+    try {
+        console.log(`Heartbeat sent successfully: ${client.ws.ping}`);
+    } catch (error) {
+        console.error('Error sending heartbeat:', error);
+    }
+}
 
 module.exports = {
     name: Events.ClientReady,
-    once: true,
-    async execute(client) {
+    once: false,
+    execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+        console.log(`WebSocket Status: ${client.ws.status}`);
+        setInterval(() => sendHeartbeat(client), heartbeatInterval);
     },
 };
