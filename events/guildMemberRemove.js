64d8f672e60cfb2896bec9c8d59ed921052aed9a
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-06 20:41:24 -0500

    convert to ES6 module

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,37 +1,34 @@
-const {
-    Events,
-    EmbedBuilder,
-    AuditLogEvent
-} = require('discord.js');
+import { Events, EmbedBuilder, AuditLogEvent } from 'discord.js'
 
-module.exports = {
-    name: Events.GuildMemberRemove,
-    once: false,
-    async execute(member) {
-        const channel = member.guild.channels.cache.find(c => c.name === "logs")
-        const embed = new EmbedBuilder()
-            .setColor("#6a0dad")
-            .setAuthor({
-                name: `${member.user.username} <${member.id}>`,
-                iconURL: member.user.avatarURL() ? member.user.avatarURL() : member.user.defaultAvatarURL
-            })
-            .setTimestamp();
+const GuildMemberRemove = {
+	name: Events.GuildMemberRemove,
+	once: false,
+	async execute(member) {
+                console.log("user left")
+                const channel = member.guild.channels.cache.find(c => c.name === "development")
+                const embed = new EmbedBuilder()
+                .setColor("#6a0dad")
+                .setAuthor({ name: `${member.user.username} <${member.id}>`, iconURL: member.user.avatarURL() ? member.user.avatarURL() : member.user.defaultAvatarURL})
+                .setTimestamp();
+                
+                const fetchedLogs = await member.guild.fetchAuditLogs({
+                        type: AuditLogEvent.MemberKick,
+                });
+                
+                fetchedLogs.entries
+                .sort((a, b) => b.createdAt - a.createdAt) 
+                .filter(a => a.targetId === member.id)
 
-        const fetchedLogs = await member.guild.fetchAuditLogs({
-            type: AuditLogEvent.MemberKick,
-        });
-
-        fetchedLogs.entries
-            .sort((a, b) => b.createdAt - a.createdAt)
-            .filter(a => a.targetId === member.id)
-
-        if (fetchedLogs.entries.size >= 1) {
-            const firstEntry = fetchedLogs.entries.first().createdAt;
-            if (firstEntry > member.joinedAt)
-                return;
-        }
-        channel.send({
-            embeds: [embed]
-        });
-    },
+                
+                console.log(fetchedLogs.entries.size)
+                if (fetchedLogs.entries.size >= 1){
+                        const firstEntry = fetchedLogs.entries.first().createdAt;
+                        console.log(`user kicked: ${firstEntry > member.joinedAt}`)
+                        if (firstEntry > member.joinedAt) 
+                                return;
+                } 
+                channel.send({ embeds: [embed] });
+	},
 };
+
+export default GuildMemberRemove
