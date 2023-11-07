34e631949c72ed9fc3170ab67ae14e75ac15c2a5
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-06 20:38:16 -0500

    convert to ES6 module

--- events/guildAuditLogEntryCreate.js
+++ events/guildAuditLogEntryCreate.js
@@ -1,54 +1,36 @@
-const {
-    AuditLogEvent,
-    Events,
-    EmbedBuilder
-} = require("discord.js");
+import { AuditLogEvent, Events, EmbedBuilder } from 'discord.js'
 
-module.exports = {
-    name: Events.GuildAuditLogEntryCreate,
-    once: false,
-    async execute(auditLog, server) {
-        const {
-            action,
-            executorId,
-            targetId
-        } = auditLog;
-        const executor = await server.members.fetch(executorId);
-        const target = await server.members.client.users.fetch(targetId);
-        const channel = server.channels.cache.find(c => c.name === "logs");
+const GuildAuditLogEntryCreate = {
+	name: Events.GuildAuditLogEntryCreate,
+	once: false,
+	async execute(auditLog, server) {
+        const { action, executorId, targetId } = auditLog
+        const executor = await server.members.fetch(executorId)
+        const target = await server.members.client.users.fetch(targetId)
+        const channel = server.channels.cache.find(c => c.name === "development")
         if (action === AuditLogEvent.MemberKick || action == auditLog.MemberBanAdd) {
             const embed = new EmbedBuilder()
-                .setAuthor({
-                    name: `${executor.user.username} <${executor.id}>`,
-                    iconURL: executor.user.avatarURL()
-                })
-                .addFields({
-                    name: 'User:',
-                    value: `${target.username} <${target.id}>`
-                }, {
-                    name: 'Reason:',
-                    value: auditLog.reason ? auditLog.reason : "no reason specified"
-                }, )
-                .setTimestamp()
-
+            .setAuthor({ name: `${executor.user.username} <${executor.id}>`, iconURL: executor.user.avatarURL() })
+            .addFields(
+                { name: 'User:', value: `${target.username} <${target.id}>` },
+                { name: 'Reason:', value: auditLog.reason ? auditLog.reason : "no reason specified" },
+            )
+            .setTimestamp()
+            
             if (action === AuditLogEvent.MemberKick) {
-                embed.setFooter({
-                    text: 'Kicked'
-                });
-                embed.setColor("#FFFF00");
+                embed.setFooter({ text: 'Kicked'})
+                embed.setColor("#FFFF00")
             }
 
             if (action === AuditLogEvent.MemberBanAdd) {
-                embed.setFooter({
-                    text: 'Banned'
-                });
-                embed.setColor("#FF0000");
+                embed.setFooter({ text: 'Banned'})
+                embed.setColor("#FF0000")
             }
-
-            channel.send({
-                embeds: [embed]
-            });
-
+            
+            channel.send({ embeds: [embed] })
+            
         }
-    },
-};
+	},
+}
+
+export default GuildAuditLogEntryCreate
