5a5ff803437bb8b1a65cbd0c29f1ee0f66de88de
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-06 20:44:46 -0500

    convert to ES6 module

--- commands/register.js
+++ commands/register.js
@@ -1,16 +1,31 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js'
+import 'dotenv/config'
 
-module.exports = {
+const register = {
 	data: new SlashCommandBuilder()
 		.setName("register")
 		.setDescription("Adds tournament role for participating users."),
 	async execute(interaction) {
-		const channel = interaction.guild.channels.cache.find(c => c.name === "logs")
-		const roleID = "823367185048928268"
-		if (interaction.member.roles.cache.some(role => role.id === roleID)) {
-			await interaction.reply("You are already registered!")
+		if (process.env.REGISTRATION === "1") {
+			const channel = interaction.guild.channels.cache.find(c => c.name === "development")
+			const roleID = "823367185048928268"
+			if (interaction.member.roles.cache.some(role => role.id === roleID)) {
+				await interaction.reply("You are already registered")
+			} else {
+				const embed = new EmbedBuilder()
+					.setColor("#FFA500")
+					.setAuthor({ name: `${interaction.member.user.username} <${interaction.member.id}>`, iconURL: interaction.member.user.avatarURL() ? interaction.member.user.avatarURL() : interaction.member.user.defaultAvatarURL })
+					.setTimestamp()
+					.setFooter({ text: "Registered for LCS 🏆" });
+				await channel.send({ embeds: [embed] });
+				await interaction.reply("foo")
+				await interaction.deleteReply()
+				await interaction.member.roles.add(roleID);
+			}
 		} else {
-			await interaction.reply("Registration has ended!")
+			await interaction.reply("No tournaments are available")
 		}
 	},
 };
+
+export default register
