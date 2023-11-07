d3ff87183871ff685bde5e09eb7911257a874353
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-06 20:40:09 -0500

    convert to ES6 module

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,28 +1,17 @@
 import { Events, EmbedBuilder } from 'discord.js'
 
-const GuildBanAdd = {
-    name: Events.GuildBanAdd,
-    once: false,
-    execute(ban) {
-        const channel = ban.guild.channels.cache.find(c => c.name === "logs");
+const GuildMemberAdd = {
+	name: Events.GuildMemberAdd,
+	once: false,
+	execute(member) {
+		const channel = member.guild.channels.cache.find(c => c.name === "development")
         const embed = new EmbedBuilder()
-            .setAuthor({
-                name: `${ban.user.username} <${ban.user.id}>`,
-                iconURL: ban.user.avatarURL()
-            })
-            .addFields({
-                name: 'Reason:',
-                value: `${ban.reason}`
-            })
-            .setColor("#FF0000")
-            .setTimestamp()
-            .setFooter({
-                text: 'Banned'
-            });  
-        channel.send({
-            embeds: [embed]
-        });
-    },
+        .setColor("008000")
+        .setAuthor({ name: `${member.user.username} <${member.id}>`, iconURL: member.user.avatarURL() ? member.user.avatarURL() : member.user.defaultAvatarURL})
+        .setTimestamp()
+        .setFooter({ text: "Joined"});
+        channel.send({ embeds: [embed] });
+	},
 };
 
-export default GuildBanAdd
+export default GuildMemberAdd
