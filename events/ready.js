5eab60d2a9380b6e110eb4c9baa7aa45d0736dbe
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-15 17:56:08 -0500

    delete unresolved challenge requests if bot restarts

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,41 @@
-import { Events } from 'discord.js'
+import { Events, Client, ActivityType, EmbedBuilder } from 'discord.js'
 
 const ready = {
 	name: Events.ClientReady,
-	once: false,
+	once: true,
+	/**
+	 * Ready message when the bot connects to the Discord API.
+	 *
+	 * @param {Client} client The main hub for interacting with the Discord API, and the starting point for any bot.
+	 */
 	async execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
-};
+		console.log(`Ready! Logged in as ${client.user.tag}`)
+		client.user.setActivity('crossyoff.rf.gd', { type: ActivityType.Watching })
+		const serverID = "600865413890310155"
+		const challengeLogsChannelID = "1171571198023442535"
+		const guild = client.guilds.cache.get(serverID)
+		const logsChannel = guild.channels.cache.get(challengeLogsChannelID)
+		const messages = await logsChannel.messages.fetch({limit: 30})
+		// delete unresolved pending challenges when bot restarts
+		const challenges = messages.filter(m => 
+			m.embeds.length === 1 &&
+			m.reactions.cache.size > 0
+		)
+		if (challenges.size > 0) {
+			challenges.forEach(async challenge => {
+				const embed = new EmbedBuilder()
+                .setColor("Red")
+                .setAuthor({
+                    name: `${client.user.username} <${client.user.id}>`,
+                    iconURL: client.user.avatarURL() ? client.user.avatarURL() : client.user.defaultAvatarURL
+                })
+                .setTimestamp()
+				.setFooter({ text: `Cancelled pending challenge ID: ${challenge.id} due to bot restart` })
+				await challenge.delete({timeout: 1000})
+				await logsChannel.send({ embeds: [embed] })
+			})
+		}
+	}
+}
 
 export default ready
