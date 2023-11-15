abff928f0cdb20ead07aaa0189fa0d73a611e504
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-15 18:37:53 -0500

    add jsdoc, color is now color resolvable

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,16 +1,22 @@
-import { Events, EmbedBuilder } from 'discord.js'
+import { Events, EmbedBuilder, GuildMember } from 'discord.js'
 
 const GuildMemberAdd = {
 	name: Events.GuildMemberAdd,
 	once: false,
-	execute(member) {
-		const channel = member.guild.channels.cache.find(c => c.name === "logs")
+    /**
+     * Emitted whenever a user joins a guild.
+     *
+     * @param {GuildMember} member The member that has joined a guild.
+     */
+	async execute(member) {
+		const logs = "682109939950288954"
+        const channel = member.guild.channels.cache.get(logs)
         const embed = new EmbedBuilder()
-        .setColor("008000")
+        .setColor("Green")
         .setAuthor({ name: `${member.user.username} <${member.id}>`, iconURL: member.user.avatarURL() ? member.user.avatarURL() : member.user.defaultAvatarURL})
         .setTimestamp()
-        .setFooter({ text: "Joined"});
-        channel.send({ embeds: [embed] });
+        .setFooter({ text: "Joined"})
+        await channel.send({ embeds: [embed] })
 	},
 };
 
