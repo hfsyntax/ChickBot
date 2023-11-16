94309eb42ea3dc502d251aa354d4539cd31b1c52
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-15 22:31:29 -0500

    define variables for readability and make embed color resolvable

--- commands/unregister.js
+++ commands/unregister.js
@@ -1,24 +1,35 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js'
+import 'dotenv/config'
 
-module.exports = {
+const unregister = {
 	data: new SlashCommandBuilder()
 		.setName("unregister")
 		.setDescription("Removes tournament role for participating users."),
 	async execute(interaction) {
-		const channel = interaction.guild.channels.cache.find(c => c.name === "logs")
-		const roleID = "823367185048928268"
-		if (!interaction.member.roles.cache.some(role => role.id === roleID)) {
-			await interaction.reply("You have not registered!")
+		if (process.env.REGISTRATION === "1") {
+			const logs = interaction.guild.channels.cache.get("682109939950288954")
+			const tournamentRoleID = process.env.TOURNAMENT_ROLE_ID
+			const tournament = process.env.TOURNAMENT_NAME
+			const username = interaction.member.user.username
+			const userID = interaction.member.id
+			const avatarURL = interaction.member.user.avatarURL() ? interaction.member.user.avatarURL() : interaction.member.user.defaultAvatarURL
+			if (!interaction.member.roles.cache.has(tournamentRoleID)) {
+				await interaction.reply(`You have not registered for ${tournament}`)
+			} else {
+				const embed = new EmbedBuilder()
+					.setColor("DarkOrange")
+					.setAuthor({ name: `${username} <${userID}>`, iconURL: avatarURL })
+					.setTimestamp()
+					.setFooter({ text: `Withdrew from ${tournament} 🏆` })
+				await logs.send({ embeds: [embed] })
+				await interaction.reply("foo")
+				await interaction.deleteReply()
+				await interaction.member.roles.remove(tournamentRoleID)
+			}
 		} else {
-			const embed = new EmbedBuilder()
-			.setColor("#964B00")
-			.setAuthor({ name: `${interaction.member.user.username} <${interaction.member.id}>`, iconURL: interaction.member.user.avatarURL() ? interaction.member.user.avatarURL() : interaction.member.user.defaultAvatarURL})
-			.setTimestamp()
-			.setFooter({ text: "Withdrew from LCS 🏆"});
-			await channel.send({ embeds: [embed] });
-			await interaction.reply("foo")
-			await interaction.deleteReply()
-			await interaction.member.roles.remove(roleID);
+			await interaction.reply("No tournaments are available")
 		}
-	},
-};
+	}
+}
+
+export default unregister
