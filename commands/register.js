58be6f97bd221e892c3dd288de8026587efd2c83
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-15 22:23:18 -0500

    add role id as environment variable

--- commands/register.js
+++ commands/register.js
@@ -8,12 +8,12 @@ const register = {
 	async execute(interaction) {
 		if (process.env.REGISTRATION === "1") {
 			const logs = interaction.guild.channels.cache.get("682109939950288954")
-			const roleID = "823367185048928268"
+			const tournamentRoleID = process.env.TOURNAMENT_ROLE_ID
 			const tournament = process.env.TOURNAMENT_NAME
 			const username = interaction.member.user.username
 			const userID = interaction.member.id
 			const avatarURL = interaction.member.user.avatarURL() ? interaction.member.user.avatarURL() : interaction.member.user.defaultAvatarURL
-			if (interaction.member.roles.cache.has(roleID)) {
+			if (interaction.member.roles.cache.has(tournamentRoleID)) {
 				await interaction.reply(`You are already registered for ${tournament}`)
 			} else {
 				const embed = new EmbedBuilder()
@@ -23,7 +23,7 @@ const register = {
 					.setFooter({ text: `Registered for ${tournament} 🏆` })
 				await logs.send({ embeds: [embed] })
 				await interaction.reply(`Successfully registered for ${tournament}`)
-				await interaction.member.roles.add(roleID)
+				await interaction.member.roles.add(tournamentRoleID)
 			}
 		} else {
 			await interaction.reply("No tournaments are available")
