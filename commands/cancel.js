212d0a49109d86fba7ac8618ea00629f745c7879
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-19 17:20:04 -0500

    Update cancel.js
     -allow refs to delete started challenges that aren't theirs
     -keep challenge embed for logging instead of deleting

--- commands/cancel.js
+++ commands/cancel.js
@@ -8,6 +8,7 @@ const cancel = {
         const challengeLog = interaction.guild.channels.cache.get("1171571198023442535")
         const playing = "1172359960559108116"
         const queued = "1172360108307644507"
+        const referee = "799505175541710848"
         const embed = new EmbedBuilder()
             .setColor("Red")
             .setAuthor({
@@ -15,30 +16,52 @@ const cancel = {
                 iconURL: interaction.user.avatarURL() ? interaction.user.avatarURL() : interaction.user.defaultAvatarURL
             })
             .setTimestamp()
-        if (interaction.member.roles.cache.has(playing)) {
-            await interaction.reply("You cannot cancel a challenge when you are already playing.")
-        }
-        else if (interaction.member.roles.cache.has(queued)) {
-            await interaction.member.roles.remove(queued)
-            await interaction.reply("You've been removed from the queue of players waiting for a challenge.")
-            embed.setFooter({ text: "Cancelled qeue for challenge" })
-            await challengeLog.send({ embeds: [embed] })
-        } else {
-            const messages = await challengeLog.messages.fetch({ limit: 30 })
-            const challenge = messages.filter(m =>
-                m.embeds.length === 1 &&
-                m.embeds[0].data.author.name &&
-                m.embeds[0].data.author.name.split('<')[1].split('>')[0]
-                === interaction.member.id &&
-                m.reactions.cache.size > 0
-            )
-            if (challenge.size > 0) {
-                embed.setFooter({ text: `Cancelled challenge ID: ${challenge.first().id}` })
-                await challengeLog.send({ embeds: [embed] })
-                await challenge.first().delete({ timeout: 1000 })
-                await interaction.reply("Sucessfully cancelled your challenge.")
+        if (interaction.channel.name.includes("challenge")) {
+            if (interaction.member.roles.cache.has(referee)) {
+                const challengeID = interaction.channel.name.split("-")[1]
+                const challengeEmbed = await challengeLog.messages.fetch(challengeID)
+                const challengerID = challengeEmbed.embeds[0].data.author.name.split('<')[1].split('>')[0]
+                const opponentID = challengeEmbed.embeds[0].data.description.split('<')[1].split('>')[0]
+                if (interaction.member.id === challengerID || interaction.member.id === opponentID) {
+                    await interaction.reply("You cannot cancel a challenge you are already playing.")
+                } else {
+                    const challenger = interaction.channel.members.cache.get(challengerID)
+                    console.log(challenger)
+                    return
+                    const opponent = ""
+                    embed.setFooter({ text: `Referee cancelled challenge ID: ${challengeID}` })
+                    await challengeLog.send({ embeds: [embed] })
+                    await interaction.reply(`Sucessfully cancelled challenge ID: ${challengeID}.`)
+                    await interaction.channel.delete({timeout: 1000})
+                }
             } else {
-                await interaction.reply("You have not created a challenge and you are not in a queue.")
+                await interaction.reply("You cannot cancel a challenge you are already playing.")
+            }
+        } else {
+            if (interaction.member.roles.cache.has(playing)) {
+                await interaction.reply("You cannot cancel a challenge when you are already playing.")
+            }
+            else if (interaction.member.roles.cache.has(queued)) {
+                await interaction.member.roles.remove(queued)
+                await interaction.reply("You've been removed from the queue of players waiting for a challenge.")
+                embed.setFooter({ text: "Cancelled qeue for challenge" })
+                await challengeLog.send({ embeds: [embed] })
+            } else {
+                const messages = await challengeLog.messages.fetch({ limit: 30 })
+                const challenge = messages.filter(m =>
+                    m.embeds.length === 1 &&
+                    m.embeds[0].data.author.name &&
+                    m.embeds[0].data.author.name.split('<')[1].split('>')[0]
+                    === interaction.member.id &&
+                    m.reactions.cache.size > 0
+                )
+                if (challenge.size > 0) {
+                    embed.setFooter({ text: `Cancelled challenge ID: ${challenge.first().id}` })
+                    await challengeLog.send({ embeds: [embed] })
+                    await interaction.reply("Sucessfully cancelled your challenge.")
+                } else {
+                    await interaction.reply("You have not created a challenge and you are not in a queue.")
+                }
             }
         }
     }
