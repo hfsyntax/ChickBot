47fdbc799e48eda42eb7b89ea8409623b39ff08e
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2023-11-19 19:56:10 -0500

    convert to an ES6 module

--- keep_alive.js
+++ keep_alive.js
@@ -1,6 +1,10 @@
-// used for web-servers that have limited runtime
-var http = require('http');
-http.createServer(function (req, res) {
-  res.write("I'm alive");
-  res.end();
-}).listen(8080);
+import http from 'http'
+
+const server = http.createServer(function (req, res) {
+  res.write("I'm alive")
+  res.end()
+})
+
+server.listen(8080)
+
+export default server
