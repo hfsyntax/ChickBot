853da6c4a51c21efb7c23a74ee99be0d80f51428
Noah Kaiser <52133373+hfsyntax@users.noreply.github.com>
2024-01-19 09:11:38 -0500

    Create keep_alive.js

---
+++ utilities/keep_alive.js
@@ -0,0 +1,14 @@
+import http from 'http'
+
+const server = http.createServer(function (req, res) {
+  res.write("I'm alive")
+  res.end()
+})
+
+const PORT = process.env.PORT || 8080;
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+export default server
