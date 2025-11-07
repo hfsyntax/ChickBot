a43c4bf5b27bde3827056e39ea0972ac8b817377
Noah Kaiser <noahkingkaiser@gmail.com>
2025-11-07 16:58:22 -0500

    remove CRC participant role on reaction remove

---
+++ src/events/messageReactionRemove.ts
@@ -0,0 +1,72 @@
+import type {
+  MessageReaction,
+  User,
+  MessageReactionEventDetails,
+} from "discord.js"
+import { Events } from "discord.js"
+import {
+  limiter,
+  memberFetchLimiter,
+  messageFetchLimiter,
+  roleRemoveLimiter,
+} from "../utilities/limiter"
+
+const MessageReactionRemove = {
+  name: Events.MessageReactionRemove,
+  once: false,
+  /**
+   * Emitted whenever a user removes a reaction.
+   *
+   */
+  async execute(
+    messageReaction: MessageReaction,
+    user: User,
+    details: MessageReactionEventDetails
+  ) {
+    const reactionMessage = messageReaction.message.partial
+      ? await messageFetchLimiter
+          .key(messageReaction.message.channel.id)
+          .schedule(() =>
+            limiter.schedule(() => messageReaction.message.fetch())
+          )
+          .catch((error) => {
+            console.error(error)
+            return null
+          })
+      : messageReaction.message
+
+    if (
+      !reactionMessage?.guild ||
+      reactionMessage.guild.id !== process.env.CRC_SERVER_ID ||
+      reactionMessage.id !== "1436302473735307337" ||
+      messageReaction.emoji.name !== "⭐"
+    )
+      return
+
+    const roleId = "1436299759492272140"
+    const member =
+      reactionMessage.guild.members.cache.get(user.id) ??
+      (await memberFetchLimiter
+        .key(process.env.CRC_SERVER_ID)
+        .schedule(() =>
+          limiter.schedule(() => {
+            if (!reactionMessage.guild) throw new Error("no guild")
+            return reactionMessage.guild.members.fetch(user.id)
+          })
+        )
+        .catch(() => null))
+
+    if (!member) return
+
+    const hasRole = member.roles.cache.has(roleId)
+
+    if (!hasRole) return
+
+    await roleRemoveLimiter
+      .key(member.guild.id)
+      .schedule(() => limiter.schedule(() => member.roles.remove(roleId)))
+      .catch(() => null)
+  },
+}
+
+export default MessageReactionRemove
